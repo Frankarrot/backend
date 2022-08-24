167a4e7073c07805bbeea6deda59c432503e3c28
Hyunta <mohai2618@naver.com>
2022-08-24 17:21:09 +0900

    initial commit

---
+++ src/main/java/com/frankarrot/FrankarrotApplication.java
@@ -0,0 +1,13 @@
+package com.frankarrot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrankarrotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrankarrotApplication.class, args);
+    }
+
+}
